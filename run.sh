0ff0b469f1fe2baa201ffe91f905407acbae28a6
den-taku <dentaku1412@gmail.com>
2022-01-22 14:36:02 +0900

    Implememt as Unix program

---
+++ run.sh
@@ -0,0 +1,139 @@
+#! /bin/tcsh -f
+
+# set path = (/sbin /bin /usr/sbin /usr/bin /usr/local/sbin /usr/local/bin $HOME/bin $HOME/.cargo/bin)
+
+# for setting up
+mkdir -p results
+mkdir -p results/voluntary_immediate
+mkdir -p results/voluntary_delay
+mkdir -p results/forced_immediate
+mkdir -p results/forced_delay
+mkdir -p results/dfs
+
+# conditions
+set TESTSIZE = 100
+set TIME_LIMIT = 1800000
+
+# for simulation setting
+set TESTS = (\
+"3_3_20_10_10_x100"\
+"10"\
+"3_3_20_10_30_x100"\
+"10"\
+"3_3_30_13_10_x100"\
+"10"\
+"3_3_30_13_30_x100"\
+"10"\
+"3_3_40_15_10_x100"\
+"10"\
+"3_3_40_15_30_x100"\
+"10"\
+\
+"4_3_20_10_10_x100"\
+"13"\
+"4_3_20_10_30_x100"\
+"13"\
+"4_3_30_13_10_x100"\
+"13"\
+"4_3_30_13_30_x100"\
+"13"\
+"4_3_40_15_10_x100"\
+"13"\
+"4_3_40_15_30_x100"\
+"13"\
+\
+"4_4_20_10_10_x100"\
+"17"\
+"4_4_20_10_30_x100"\
+"17"\
+"4_4_30_13_10_x100"\
+"17"\
+"4_4_30_13_30_x100"\
+"17"\
+"4_4_40_15_10_x100"\
+"17"\
+"4_4_40_15_30_x100"\
+"17"\
+\
+"5_3_20_10_10_x100"\
+"16"\
+"5_3_20_10_30_x100"\
+"16"\
+"5_3_30_13_10_x100"\
+"16"\
+"5_3_30_13_30_x100"\
+"16"\
+"5_3_40_15_10_x100"\
+"16"\
+"5_3_40_15_30_x100"\
+"16"\
+\
+"6_3_20_10_10_x100"\
+"19"\
+"6_3_20_10_30_x100"\
+"19"\
+"6_3_30_13_10_x100"\
+"19"\
+"6_3_30_13_30_x100"\
+"19"\
+"6_3_40_15_10_x100"\
+"19"\
+"6_3_40_15_30_x100"\
+"19"\
+\
+"6_4_40_15_10_x100"\
+"25"\
+"6_4_40_15_30_x100"\
+"25"\
+"6_4_50_20_10_x100"\
+"25"\
+"6_4_50_20_30_x100"\
+"25"\
+"6_4_60_25_10_x100"\
+"25"\
+"6_4_60_25_30_x100"\
+"25"\
+"6_5_70_30_10_x100"\
+"31"\
+"6_5_70_30_30_x100"\
+"31"\
+)
+# set TESTS = ( "6_4_60_25_30_x100" )
+
+set i = 1
+set TESTS_LEN = 0
+@ TESTS_LEN = $#TESTS / 2
+while ( $i <= $TESTS_LEN )
+    # i番目のテストが実行される
+    set i0 = 1
+    @ i0 = $i * 2 - $i0
+    set i1 = 0
+    @ i1 = $i * 2
+    set INSTANCE = $TESTS[$i0]
+    set SIZE = $TESTS[$i1]
+    echo $SIZE > config
+
+    # 解く種類を指定
+
+    # mkdir -p results/forced_immediate/$INSTANCE
+    # mkdir -p results/forced_delay/$TESTS[$i]
+    # mkdir -p results/voluntary_immediate/$TESTS[$i]
+    mkdir -p results/voluntary_delay/$TESTS[$i]
+
+    # cargo build --release
+
+    set TESTCASE = 0
+    while ( $TESTCASE < $TESTSIZE )
+        # TESTCASE番目のインスタンスが解かれる
+
+        # ./target/release/bin ForcedImmediate $INSTANCE $TESTCASE $TIME_LIMIT
+        # ./target/release/bin ForcedDelay $TESTS[$i] $TESTCASE $TIME_LIMIT
+        # ./target/release/bin VoluntaryImmediate $TESTS[$i] $TESTCASE $TIME_LIMIT
+        python3 solver.py 2 2 $TESTS[$i] $TESTCASE $TIME_LIMIT
+        @ TESTCASE = $TESTCASE + 1
+    end
+    cd graduate-bencher
+    cargo run --release ../results/voluntary_delay/$INSTANCE $TESTSIZE
+    cd ..
+    @ i = $i + 1
+end
--- solver.py
+++ solver.py
@@ -1,172 +1,184 @@
 from mip import Model, minimize, BINARY, xsum, OptimizationStatus
 import sys
+import time as timer
 
 # path = "../graduate-second/second/testcase/4_3_30_15_30/testcase"
-path = "../graduate-second/second/testcase/3_3_20_10_30_x100/testcase"
+# path = "../graduate-second/second/testcase/3_3_20_10_30_x100/testcase"
 # path = "../graduate-second/second/testcase/5_3_40_15_30/testcase"
 # path = "../graduate-second/second/testcase/3_3_20_7_0/testcase"
 # path = "test"
+path = "../graduate-second/second/testcase/"
 
-result_path = "results"
-result_file = open(result_path, 'w')
-datalist = []
+# datalist = []
 
-for test_number in range(20):
-    print("solve {}\n".format(test_number))
+# run `python3 solver.py ForV IorD TESTNAME TESTCASE_INDEX TIME_LIMIT`
+# 1: Forced, 2: Voluntary
+# 1: Immediately, 2: Delay
+args = sys.argv
+f_or_v = args[1]
+i_or_d = args[2]
+test_name = args[3]
+test_index = args[4]
+time_limit = args[5]
 
-    # manage input
-    f = open(path + str(test_number), 'r')
-    data = f.read()
-    f.close()
-    stream = data.split()
 
-    # constant
-    stack_size = int(stream[0])
-    tier_size = int(stream[1])
-    horizon = int(stream[2])
-    blocks = int(stream[3])
-    filling_rate = int(stream[4])
-    initially_located = int(stream[5])
+# for test_number in range(100):
+print("solve {}\n".format(test_index))
 
-    # blocks
-    tmp_blocks = []
-    arrivings = []
-    expecteds = []
-    for i in range(initially_located):
-        tmp_blocks.append((int(stream[6 + i]), 0, i))
-    for i in range(blocks - initially_located):
-        tmp_blocks.append(
-            (int(stream[6 + initially_located + 2 * i + 1]), int(stream[6 + initially_located + 2 * i]), i + initially_located))
-    print(tmp_blocks)
-    tmp_blocks.sort()
-    for i in range(blocks):
-        arrivings.append(tmp_blocks[i][1])
-        expecteds.append(tmp_blocks[i][0])
-    print(tmp_blocks)
-    print(arrivings)
-    print(expecteds)
+# manage input
+f = open(path + str(test_name) + "/testcase" + test_index, 'r')
+data = f.read()
+f.close()
+stream = data.split()
 
-    # model
-    solver = Model("DBRP")
+# constant
+stack_size = int(stream[0])
+tier_size = int(stream[1])
+horizon = int(stream[2])
+blocks = int(stream[3])
+filling_rate = int(stream[4])
+initially_located = int(stream[5])
 
-    # variables
-    # block n is placed in slot (i, j) in a(n)
-    a = [[[[solver.add_var('a({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
-        horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
-    # block n is placed in (i, j) in t
-    b = [[[[solver.add_var('b({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
-        horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
-    # block n is relocated from (i, j) to (k, l) in t
-    x = [[[[[[solver.add_var('x({},{},{},{},{},{})'.format(i, j, k, l, n, t), var_type=BINARY) for t in range(horizon + 1)] for n in range(
-        blocks)] for l in range(tier_size)] for k in range(stack_size)] for j in range(tier_size)] for i in range(stack_size)]
-    # block n in (i, j) is retrieved in t
-    y = [[[[solver.add_var('y({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
-        horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
-    # block n is retrieved before t
-    v = [[solver.add_var('v({},{})'.format(n, t), var_type=BINARY)
-          for t in range(horizon + 1)] for n in range(blocks)]
-    # variables' constraint
-    # for a
-    for n in range(blocks):
-        for i in range(stack_size):
-            for j in range(tier_size):
-                for t in range(horizon + 1):
-                    if t != arrivings[n]:
-                        solver.add_constr(a[i][j][n][t] == 0)
-    # for b
-    for n in range(blocks):
-        for i in range(stack_size):
-            for j in range(tier_size):
-                for t in range(arrivings[n] + 1):
-                    solver.add_constr(b[i][j][n][t] == 0)
-    # for x
-    for n in range(blocks):
-        for i in range(stack_size):
-            for j in range(tier_size):
-                for k in range(stack_size):
-                    for l in range(tier_size):
-                        for t in range(horizon + 1):
-                            if (i == k) or (t <= arrivings[n]):
-                                solver.add_constr(x[i][j][k][l][n][t] == 0)
-    # for y
-    for n in range(blocks):
-        for i in range(stack_size):
-            for j in range(tier_size):
-                for t in range(expecteds[n]):
-                    solver.add_constr(y[i][j][n][t] == 0)
-    # for v
-    for n in range(blocks):
-        for t in range(expecteds[n] + 1):
-            solver.add_constr(v[n][t] == 0)
+# blocks
+tmp_blocks = []
+arrivings = []
+expecteds = []
+for i in range(initially_located):
+    tmp_blocks.append((int(stream[6 + i]), 0, i))
+for i in range(blocks - initially_located):
+    tmp_blocks.append(
+        (int(stream[6 + initially_located + 2 * i + 1]), int(stream[6 + initially_located + 2 * i]), i + initially_located))
+print(tmp_blocks)
+tmp_blocks.sort()
+for i in range(blocks):
+    arrivings.append(tmp_blocks[i][1])
+    expecteds.append(tmp_blocks[i][0])
+print(tmp_blocks)
+print(arrivings)
+print(expecteds)
 
-    # objective function
-    solver.objective = minimize(xsum(x[i][j][k][l][n][t] for i in range(stack_size) for j in range(
-        tier_size) for k in range(stack_size) for l in range(tier_size) for n in range(blocks) for t in range(horizon+1)))
+# model
+solver = Model("DBRP")
 
-    # constraint
-
-    # 0. initial conditions
-    for n in range(blocks):
-        initial_number = tmp_blocks[n][2]
-        if initial_number < initially_located:
-            solver.add_constr(a[initial_number % stack_size][int(initial_number / stack_size)]
-                              [n][0] == 1, 'cnst01({})'.format(n))
-            print("a[{}][{}][{}][0] == 1".format(initial_number %
-                  stack_size, int(initial_number / stack_size), n))
-
-    # 1. Slot (i, j) can hold at most one block in period t
+# variables
+# block n is placed in slot (i, j) in a(n)
+a = [[[[solver.add_var('a({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
+    horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
+# block n is placed in (i, j) in t
+b = [[[[solver.add_var('b({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
+    horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
+# block n is relocated from (i, j) to (k, l) in t
+x = [[[[[[solver.add_var('x({},{},{},{},{},{})'.format(i, j, k, l, n, t), var_type=BINARY) for t in range(horizon + 1)] for n in range(
+    blocks)] for l in range(tier_size)] for k in range(stack_size)] for j in range(tier_size)] for i in range(stack_size)]
+# block n in (i, j) is retrieved in t
+y = [[[[solver.add_var('y({},{},{},{})'.format(i, j, n, t), var_type=BINARY) for t in range(
+    horizon + 1)] for n in range(blocks)] for j in range(tier_size)] for i in range(stack_size)]
+# block n is retrieved before t
+v = [[solver.add_var('v({},{})'.format(n, t), var_type=BINARY)
+      for t in range(horizon + 1)] for n in range(blocks)]
+# variables' constraint
+# for a
+for n in range(blocks):
     for i in range(stack_size):
         for j in range(tier_size):
-            for t in range(horizon+1):
-                solver.add_constr(
-                    xsum(a[i][j][n][t] + b[i][j][n][t] for n in range(blocks)) <= 1, 'cnst1({},{},{})'.format(i, j, t))
-
-    # 2. Block n is either on the ground or on another block
+            for t in range(horizon + 1):
+                if t != arrivings[n]:
+                    solver.add_constr(a[i][j][n][t] == 0)
+# for b
+for n in range(blocks):
     for i in range(stack_size):
-        for j in range(1, tier_size):
-            for t in range(1, horizon+1):
-                solver.add_constr(xsum(
-                    a[i][j][n][t] + b[i][j][n][t] - b[i][j - 1][n][t] for n in range(blocks)) <= 0, 'cnst2({},{},{})'.format(i, j, t))
+        for j in range(tier_size):
+            for t in range(arrivings[n] + 1):
+                solver.add_constr(b[i][j][n][t] == 0)
+# for x
+for n in range(blocks):
+    for i in range(stack_size):
+        for j in range(tier_size):
+            for k in range(stack_size):
+                for l in range(tier_size):
+                    for t in range(horizon + 1):
+                        if (i == k) or (t <= arrivings[n]):
+                            solver.add_constr(x[i][j][k][l][n][t] == 0)
+# for y
+for n in range(blocks):
+    for i in range(stack_size):
+        for j in range(tier_size):
+            for t in range(expecteds[n]):
+                solver.add_constr(y[i][j][n][t] == 0)
+# for v
+for n in range(blocks):
+    for t in range(expecteds[n] + 1):
+        solver.add_constr(v[n][t] == 0)
 
-    # 3. At most one movement is possible in period t
-    for t in range(1, horizon+1):
-        solver.add_constr((xsum(x[i][j][k][l][n][t] for i in range(stack_size) for j in range(
-            tier_size) for k in range(stack_size) for l in range(tier_size) for n in range(blocks)) + xsum(a[i][j][n][t] + y[i][j][n][t] for i in range(stack_size) for j in range(tier_size) for n in range(blocks))) <= 1, 'cnst3({})'.format(t))
+# objective function
+solver.objective = minimize(xsum(x[i][j][k][l][n][t] for i in range(stack_size) for j in range(
+    tier_size) for k in range(stack_size) for l in range(tier_size) for n in range(blocks) for t in range(horizon+1)))
 
-    # 4. Block n is placed in some slot in its arrival period a(n)
-    for n in range(blocks):
-        solver.add_constr(xsum(a[i][j][n][arrivings[n]]
-                          for i in range(stack_size) for j in range(tier_size)) == 1, 'cnst4({})'.format(n))
+# constraint
 
-    # 5. Block (n + 1) cannot be retrieved before block n
-    for n in range(blocks-1):
-        solver.add_constr(xsum(v[n][t] for t in range(
-            horizon+1)) >= xsum(v[n + 1][t] for t in range(horizon+1)) + 1, 'cnst5({})'.format(n))
+# 0. initial conditions
+for n in range(blocks):
+    initial_number = tmp_blocks[n][2]
+    if initial_number < initially_located:
+        solver.add_constr(a[initial_number % stack_size][int(initial_number / stack_size)]
+                          [n][0] == 1, 'cnst01({})'.format(n))
+        print("a[{}][{}][{}][0] == 1".format(initial_number %
+              stack_size, int(initial_number / stack_size), n))
 
-    # 6. Block n is not in the bay in period t (> e(n)) iff it is retrieved before t
-    for n in range(blocks):
+# 1. Slot (i, j) can hold at most one block in period t
+for i in range(stack_size):
+    for j in range(tier_size):
         for t in range(horizon+1):
-            solver.add_constr(v[n][t] == xsum(y[i][j][n][s] for i in range(
-                stack_size) for j in range(tier_size) for s in range(t)), 'cnst6({},{})'.format(n, t))
+            solver.add_constr(
+                xsum(a[i][j][n][t] + b[i][j][n][t] for n in range(blocks)) <= 1, 'cnst1({},{},{})'.format(i, j, t))
 
-    # 7. All the blocks must be retrieved in the planning horizon
-    solver.add_constr(xsum(y[i][j][n][t] for i in range(stack_size) for j in range(
-        tier_size) for n in range(blocks) for t in range(horizon+1)) == blocks, 'cnst7')
+# 2. Block n is either on the ground or on another block
+for i in range(stack_size):
+    for j in range(1, tier_size):
+        for t in range(1, horizon+1):
+            solver.add_constr(xsum(
+                a[i][j][n][t] + b[i][j][n][t] - b[i][j - 1][n][t] for n in range(blocks)) <= 0, 'cnst2({},{},{})'.format(i, j, t))
 
-    # 8. Block n can take only three states in period t: not yet arrived, placed, have been retrieved
-    for n in range(blocks):
-        for t in range(arrivings[n] + 1, horizon+1):
-            solver.add_constr(xsum(b[i][j][n][t]  # + v[n][t]
-                              for i in range(stack_size) for j in range(tier_size)) + v[n][t] == 1, 'cnst8({},{})'.format(n, t))
+# 3. At most one movement is possible in period t
+for t in range(1, horizon+1):
+    solver.add_constr((xsum(x[i][j][k][l][n][t] for i in range(stack_size) for j in range(
+        tier_size) for k in range(stack_size) for l in range(tier_size) for n in range(blocks)) + xsum(a[i][j][n][t] + y[i][j][n][t] for i in range(stack_size) for j in range(tier_size) for n in range(blocks))) <= 1, 'cnst3({})'.format(t))
 
-    # 9. The state of slot (i, j) in period t is determined
-    for i in range(stack_size):
-        for j in range(tier_size):
-            for n in range(blocks):
-                for t in range(arrivings[n] + 1, horizon+1):
-                    solver.add_constr(b[i][j][n][t] == b[i][j][n][t - 1] + xsum(x[k][l][i][j][n][t - 1] - x[i][j][k][l][n][t - 1] for k in
-                                      range(stack_size) for l in range(tier_size)) - y[i][j][n][t - 1] + a[i][j][n][t - 1], 'cnst9({},{},{},{})'.format(i, j, n, t))
+# 4. Block n is placed in some slot in its arrival period a(n)
+for n in range(blocks):
+    solver.add_constr(xsum(a[i][j][n][arrivings[n]]
+                      for i in range(stack_size) for j in range(tier_size)) == 1, 'cnst4({})'.format(n))
 
+# 5. Block (n + 1) cannot be retrieved before block n
+for n in range(blocks-1):
+    solver.add_constr(xsum(v[n][t] for t in range(
+        horizon+1)) >= xsum(v[n + 1][t] for t in range(horizon+1)) + 1, 'cnst5({})'.format(n))
+
+# 6. Block n is not in the bay in period t (> e(n)) iff it is retrieved before t
+for n in range(blocks):
+    for t in range(horizon+1):
+        solver.add_constr(v[n][t] == xsum(y[i][j][n][s] for i in range(
+            stack_size) for j in range(tier_size) for s in range(t)), 'cnst6({},{})'.format(n, t))
+
+# 7. All the blocks must be retrieved in the planning horizon
+solver.add_constr(xsum(y[i][j][n][t] for i in range(stack_size) for j in range(
+    tier_size) for n in range(blocks) for t in range(horizon+1)) == blocks, 'cnst7')
+
+# 8. Block n can take only three states in period t: not yet arrived, placed, have been retrieved
+for n in range(blocks):
+    for t in range(arrivings[n] + 1, horizon+1):
+        solver.add_constr(xsum(b[i][j][n][t]  # + v[n][t]
+                          for i in range(stack_size) for j in range(tier_size)) + v[n][t] == 1, 'cnst8({},{})'.format(n, t))
+
+# 9. The state of slot (i, j) in period t is determined
+for i in range(stack_size):
+    for j in range(tier_size):
+        for n in range(blocks):
+            for t in range(arrivings[n] + 1, horizon+1):
+                solver.add_constr(b[i][j][n][t] == b[i][j][n][t - 1] + xsum(x[k][l][i][j][n][t - 1] - x[i][j][k][l][n][t - 1] for k in
+                                  range(stack_size) for l in range(tier_size)) - y[i][j][n][t - 1] + a[i][j][n][t - 1], 'cnst9({},{},{},{})'.format(i, j, n, t))
+
+if i_or_d == "1":
     # 10. The target must be retrieved if on the top
     for i in range(stack_size):
         for j in range(tier_size):
@@ -179,32 +191,54 @@ for test_number in range(20):
                         solver.add_constr(b[i][j][n][t] + xsum(v[block][t]
                                           for block in range(n)) - n <= y[i][j][n][t] + xsum(a[stack][tier][block][t] for stack in range(stack_size) for tier in range(tier_size) for block in range(blocks)))
 
-    status = solver.optimize(max_seconds=18000)
+if f_or_v == "1":
+    print("forced")
 
-    print("\n{}\n".format(solver.status))
-    if solver.status == OptimizationStatus.OPTIMAL:
-        ans = 0
-        for i in range(stack_size):
-            for j in range(tier_size):
-                for k in range(stack_size):
-                    if k == i:
-                        continue
-                    for l in range(tier_size):
-                        for n in range(blocks):
-                            for t in range(horizon + 1):
-                                if x[i][j][k][l][n][t].x >= 0.99:
-                                    ans += 1
-        print("ans: {}\n".format(ans))
-        datalist.append("{}\n".format(ans))
-    elif solver.status == OptimizationStatus.INFEASIBLE:
-        datalist.append("INFEASIBLE\n")
-    else:
-        datalist.append("FAIL\n")
+start = timer.time()
+status = solver.optimize(max_seconds=int(time_limit))
+end = timer.time()
 
-    # solver.write('models/model{}.lp'.format(test_number))
+time = int(end - start)
 
-    if test_number % 5 == 4:
-        datalist.append('\n')
+# oom time_limit time feasible relocations lb ub nodes cached
+print("\n{}\n".format(solver.status))
 
+if f_or_v == "1" and i_or_d == "1":
+    result_path = "results/forced_immediate/" + \
+        str(test_name) + "/result" + test_index
+elif f_or_v == "1" and i_or_d == "2":
+    result_path = "results/forced_delay/" + \
+        str(test_name) + "/result" + test_index
+elif f_or_v == "2" and i_or_d == "2":
+    result_path = "results/voluntary_delay/" + \
+        str(test_name) + "/result" + test_index
+else:
+    result_path = "results/voluntary_immediate/" + \
+        str(test_name) + "/result" + test_index
+
+result_file = open(result_path, 'w')
+datalist = []
+if solver.status == OptimizationStatus.OPTIMAL:
+    ans = 0
+    for i in range(stack_size):
+        for j in range(tier_size):
+            for k in range(stack_size):
+                if k == i:
+                    continue
+                for l in range(tier_size):
+                    for n in range(blocks):
+                        for t in range(horizon + 1):
+                            if x[i][j][k][l][n][t].x >= 0.99:
+                                ans += 1
+    print("1 1 {} 1 {} Some(0) Some(0) 0 0\n".format(time, ans))
+    datalist.append("1 1 {} 1 {} Some(0) Some(0) 0 0\n".format(time, ans))
+elif solver.status == OptimizationStatus.INFEASIBLE:
+    datalist.append("1 1 {} 0 123456 None None 0 0\n".format(time))
+else:
+    datalist.append("1 0 123456789 0 123456789 None None 0 0\n")
+
+# solver.write('models/model{}.lp'.format(test_number))
+
+# end
 result_file.writelines(datalist)
 result_file.close()
